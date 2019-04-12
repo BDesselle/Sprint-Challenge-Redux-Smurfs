11b081b6e3ecaead75849445d85eb6c13880f76c
Brandon Desselle <desselle.brandon2@gmail.com>
2019-04-12 12:32:20 -0500

    Update App.js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,73 @@
-import React, { Component } from 'react';
-import './App.css';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash, faUserEdit } from '@fortawesome/free-solid-svg-icons';
+
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { postSmurf, fetchSmurfs, deleteSmurf, updateSmurf, showForm } from "../actions";
+import AddSmurf from "./AddSmurf";
+import UpdateSmurf from "./UpdateSmurf"
+import Smurf from "./Smurf";
+
+import "./App.css";
+
+library.add(faTrash, faUserEdit);
+
 class App extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  componentDidMount() {
+    this.props.fetchSmurfs();
+  }
+
+  postSmurf = smurf => {
+    this.props.postSmurf(smurf);
+  };
+
+  deleteSmurf = id => {
+    this.props.deleteSmurf(id);
+  };
+
+  showForm = id => {
+    this.props.showForm(id)
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        {/* render add form or update form depending on flag */}
+        {this.props.addFormVisible ? (
+          <AddSmurf smurfs={this.props.smurfs} postSmurf={this.postSmurf} />
+        ) : (
+          <UpdateSmurf smurfs={this.props.smurfs} />
+        )}
+
+        <div className="smurf-container">
+          {this.props.smurfs.map(smurf => (
+            <Smurf
+              key={smurf.id}
+              smurf={smurf}
+              deleteSmurf={this.deleteSmurf}
+              showForm={this.showForm}
+            />
+          ))}
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+const mstp = state => {
+  return {
+    smurfs: state.smurfs,
+    addFormVisible: state.addFormVisible,
+    id: state.id
+  };
+};
+
+export default connect(
+  mstp,
+  { postSmurf, fetchSmurfs, deleteSmurf, updateSmurf, showForm }
+)(App);
